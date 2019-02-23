5ec21fd3429050b1038f4099f174155dcc933d3b
Hiver <Hiver@Inbarajs-MacBook-Air.local>
2019-02-23 12:00:46 +0530

    finish first draft of the game

--- index.js
+++ index.js
@@ -19,12 +19,13 @@
 
 const grid = [];
 const GRID_LENGTH = 3;
+const winnerBanner = document.querySelector(".winner");
 let turn = 'X';
 
 function initializeGrid() {
-    for (let colIdx = 0;colIdx < GRID_LENGTH; colIdx++) {
+    for (let colIdx = 0; colIdx < GRID_LENGTH; colIdx++) {
         const tempArray = [];
-        for (let rowidx = 0; rowidx < GRID_LENGTH;rowidx++) {
+        for (let rowidx = 0; rowidx < GRID_LENGTH; rowidx++) {
             tempArray.push(0);
         }
         grid.push(tempArray);
@@ -33,22 +34,22 @@ function initializeGrid() {
 
 function getRowBoxes(colIdx) {
     let rowDivs = '';
-    
-    for(let rowIdx=0; rowIdx < GRID_LENGTH ; rowIdx++ ) {
+
+    for (let rowIdx = 0; rowIdx < GRID_LENGTH; rowIdx++) {
         let additionalClass = 'darkBackground';
         let content = '';
         const sum = colIdx + rowIdx;
-        if (sum%2 === 0) {
+        if (sum % 2 === 0) {
             additionalClass = 'lightBackground'
         }
         const gridValue = grid[colIdx][rowIdx];
-        if(gridValue === 1) {
+        if (gridValue === 1) {
             content = '<span class="cross">X</span>';
         }
         else if (gridValue === 2) {
             content = '<span class="cross">O</span>';
         }
-        rowDivs = rowDivs + '<div colIdx="'+ colIdx +'" rowIdx="' + rowIdx + '" class="box ' +
+        rowDivs = rowDivs + '<div colIdx="' + colIdx + '" rowIdx="' + rowIdx + '" class="box ' +
             additionalClass + '">' + content + '</div>';
     }
     return rowDivs;
@@ -56,7 +57,7 @@ function getRowBoxes(colIdx) {
 
 function getColumns() {
     let columnDivs = '';
-    for(let colIdx=0; colIdx < GRID_LENGTH; colIdx++) {
+    for (let colIdx = 0; colIdx < GRID_LENGTH; colIdx++) {
         let coldiv = getRowBoxes(colIdx);
         coldiv = '<div class="rowStyle">' + coldiv + '</div>';
         columnDivs = columnDivs + coldiv;
@@ -71,12 +72,139 @@ function renderMainGrid() {
 }
 
 function onBoxClick() {
+    // compute state of grid on each box click
+
     var rowIdx = this.getAttribute("rowIdx");
     var colIdx = this.getAttribute("colIdx");
-    let newValue = 1;
-    grid[colIdx][rowIdx] = newValue;
-    renderMainGrid();
-    addClickHandlers();
+    const currentValue = grid[colIdx][rowIdx];
+
+    // an existing grid block may not be reassigned.
+    // if the value of the box is 0, it means a move on this
+    // box hasn't been made.
+    if (currentValue === 0) {
+        turn = turn === 'X' ? 'O' : 'X';
+        let newValue = turn === 'X' ? 2 : 1;
+        grid[colIdx][rowIdx] = newValue;
+        renderMainGrid();
+        addClickHandlers();
+    }
+
+    // row check
+    for (let rowIndex = 0; rowIndex < GRID_LENGTH; rowIndex++) {
+        const currentRow = grid[rowIndex];
+
+        let playerOneWinner = currentRow.filter(number => number === 1).length === currentRow.length;
+        let computerWinner = currentRow.filter(number => number === 2).length === currentRow.length;
+
+        if (playerOneWinner) {
+            winnerBanner.classList.remove('hidden');
+            winnerBanner.innerHTML = 'Player One, Congratulations!';
+            break;
+        }
+
+        if (computerWinner) {
+            winnerBanner.classList.remove('hidden');
+            winnerBanner.innerHTML = 'Player One, sorry! The computer got you on this one.';
+            break;
+        }
+    }
+
+    for (let j = 0; j < GRID_LENGTH; j++) {
+        let winner = searchColumnsForWinner(j, grid);
+
+        if (winner > 0) {
+            displayWinner(winner);
+            break;
+        }
+    }
+
+    // search left and right diagonals
+    let left = searchLeftDiagonal();
+    let right = searchRightDiagonal();
+
+    if (left > 0) {
+        displayWinner(left);
+    } else if (right > 0) {
+        displayWinner(right);
+    }
+}
+
+/**
+ * Diplay the winner on the page
+ * @param {Number} winner 
+ */
+function displayWinner(winner) {
+    if (winner === 0) return;
+
+    const playerOneWinnerText = 'Player One, Congratulations!';
+    const computerWinnerText = 'Player One, sorry! The computer got you on this one.';
+
+    winnerBanner.classList.remove('hidden');
+    winnerBanner.innerHTML = winner === 1 ? playerOneWinnerText : computerWinnerText;
+}
+
+function searchColumnsForWinner(colIndex, grid) {
+    let itemsInCol = [];
+
+    for (let rowIndex = 0; rowIndex < grid.length; rowIndex++) {
+        const currentItem = grid[rowIndex][colIndex];
+        itemsInCol.push(currentItem);
+    }
+
+    let player = itemsInCol.filter(number => number === 1).length === GRID_LENGTH;
+    let computer = itemsInCol.filter(number => number === 2).length === GRID_LENGTH;
+
+    if (player) {
+        return 1;
+    } else if (computer) {
+        return 2;
+    } else {
+        return 0;
+    }
+}
+
+function searchLeftDiagonal() {
+    let rowIndex = 0;
+    let colIndex = 0;
+    let itemsInDiagonal = [];
+    while (rowIndex < GRID_LENGTH && colIndex < GRID_LENGTH) {
+        itemsInDiagonal.push(grid[rowIndex][colIndex]);
+        rowIndex++;
+        colIndex++;
+    }
+
+    let player = itemsInDiagonal.filter(number => number === 1).length === GRID_LENGTH;
+    let computer = itemsInDiagonal.filter(number => number === 2).length === GRID_LENGTH;
+
+    if (player) {
+        return 1;
+    } else if (computer) {
+        return 2;
+    } else {
+        return 0;
+    }
+}
+
+function searchRightDiagonal() {
+    let rowIndex = 0;
+    let colIndex = GRID_LENGTH - 1;
+    let itemsInDiagonal = [];
+    while (rowIndex < GRID_LENGTH && colIndex >= 0) {
+        itemsInDiagonal.push(grid[rowIndex][colIndex]);
+        rowIndex++;
+        colIndex--;
+    }
+
+    let player = itemsInDiagonal.filter(number => number === 1).length === GRID_LENGTH;
+    let computer = itemsInDiagonal.filter(number => number === 2).length === GRID_LENGTH;
+
+    if (player) {
+        return 1;
+    } else if (computer) {
+        return 2;
+    } else {
+        return 0;
+    }
 }
 
 function addClickHandlers() {
